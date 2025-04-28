707d2ed9de686ad4936a3f7264f69cab497cfc4a
tarcizio502 <tarciziogoncalves502@gmail.com>
2025-04-28 09:55:10 -0300

    Pequenas mudanças.

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
+function calcular(operador) {
+  let n1 = parseFloat(document.getElementById("num1").value);
+  let n2 = parseFloat(document.getElementById("num2").value);
+  let resultado = "";
 
-
-    function calcular(operador) {
-    let n1 = parseFloat(document.getElementById('num1').value);
-    let n2 = parseFloat(document.getElementById('num2').value);
-    let resultado = '';
-
-    if (isNaN(n1) || isNaN(n2)) {
-        resultado = 'Insira dois números válidos!';
-    } else {
-        switch (operador) {
-            case '+':
-                resultado = n1 + n2;
-                break;
-            case '-':
-                resultado = n1 - n2;
-                break;
-            case '*':
-                resultado = n1 * n2;
-                break;
-            case '/':
-                resultado = n2 !== 0 ? (n1 / n2) : 'Divisão por zero!';
-                break;
-        }
+  if (isNaN(n1) || isNaN(n2)) {
+    resultado = "Insira dois números válidos!";
+  } else {
+    switch (operador) {
+      case "+":
+        resultado = n1 + n2;
+        break;
+      case "-":
+        resultado = n1 - n2;
+        break;
+      case "*":
+        resultado = n1 * n2;
+        break;
+      case "/":
+        resultado = n2 !== 0 ? n1 / n2 : "Divisão por zero!";
+        break;
     }
+  }
 
-    document.getElementById('resultado').innerText = `Resultado: ${resultado}`;
+  document.getElementById("resultado").innerText = `Resultado: ${resultado}`;
 }
